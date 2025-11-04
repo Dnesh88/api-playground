892cf98453782dbcef9c929ab613e1c83fac20d0
Dinesh Sherpa <itprodinesh3@outlook.com>
2025-11-04 10:42:44 +0000

    logger added

--- simple-express-app/index.js
+++ simple-express-app/index.js
@@ -1,11 +1,28 @@
+/* server definition */
+
 const express = require("express");
 const server = express();
 const port = 8080;
 
-server.get("/", (req, res) => {
-  res.send("<h2>It works!</h2>");
-});
-
 server.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
+});
+
+/* middleware. common usage
+    - logging
+    - authentication, authorization
+*/
+
+// logger
+server.use((req, res, next) => {
+  const { url, method } = req;
+  const time = new Date();
+  const message = `${method} - ${url} - ${time}`;
+  console.log(message);
+  next();
+});
+
+/* routes */
+server.get("/", (req, res) => {
+  res.send("<h2>It works!</h2>");
 });
